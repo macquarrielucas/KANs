aee5df4fde6173586a236ec261d64da3b76c57c9
macquarrielucas <macquarrielucas@gmail.com>
2025-02-12 16:45:57 -0400

    refactored plotting functon and split the tests for 1d and 2d. currently does not return as expected

--- KANODE/UniversalDiffEq/plotting_functions.jl
+++ KANODE/UniversalDiffEq/plotting_functions.jl
@@ -1,29 +1,44 @@
 using Printf
+# This is just to suppress warnings
+using Suppressor
 
-
-#COLORS 
-test_loss_color = :tomato
-loss_color=:royalblue1
-timeseries_x_color = :lightskyblue2
-timeseries_y_color= :royalblue1
+const COLORS = (
+    test_loss = :tomato,
+    loss = :royalblue1,
+    timeseries_x = :lightskyblue2,
+    timeseries_y = :royalblue1,
+    true_solution = :black,
+    prediction = :royalblue1,
+    initial_condition = :blue,
+    training_region = :gray
+)
 dir = @__DIR__
-
-#Structure for passing data which doesn't need to be recalculated
-struct StaticData
+# Structure for passing data for the 2d plot which doesn't need to be recalculated
+struct StaticData_2D
     x::StepRangeLen
     y::StepRangeLen
-    xy::Matrix{Tuple{Float64, Float64}}
+    xy::Matrix{Tuple{Float64,Float64}}
     true_h::Matrix{Float64}
-    tspan_train::Tuple{Float64, Float64}
-    u0::Vector{Float64}
-    spinning_rate::Real
+    tspan_train::Tuple{Float32,Float32}
+    u0::Vector{Float32}
+    spinning_rate::Float32
+end
+
+# Structure for passing data for the 1d plot which doesn't need to be recalculated
+struct StaticData_1D
+    x::StepRangeLen
+    true_h::Vector{Float64}
+    tspan_train::Tuple{Float32,Float32}
+    u0::Vector{Float32}
+    spinning_rate::Float32
+    sol_max_x::Float32
 end
 
 function find_frame_directory()
     # Create directories if needed
     folder_count = 0
     training_dir = joinpath(dir, "tests", "training_frames_$folder_count")
-    #Set up the folder if it doesnt already exist
+    # Set up the folder if it doesn't already exist
     if !isdir(joinpath(dir, "tests"))
         println("Making test folder")
         mkdir(joinpath(dir, "tests"))
@@ -33,10 +48,10 @@ function find_frame_directory()
         mkdir(training_dir)
     end
     is_empty = isempty(readdir(training_dir))
-    #If its already a directory and its not empty, try the next folder
+    # If it's already a directory and it's not empty, try the next folder
     while isdir(training_dir) && !is_empty 
         folder_count += 1 
-        training_dir = joinpath("tests", "training_frames_$folder_count")
+        training_dir = joinpath(dir, "tests", "training_frames_$folder_count")
     end
     if !is_empty
         print("Making directory ", string(training_dir))
@@ -45,116 +60,110 @@ function find_frame_directory()
     training_dir
 end
 
-function save_training_frame_2d(static_data::StaticData, model::UDE, nn, pM, iter, loss, test_loss, training_dir; save=false)
+function plot_loss(plt, loss, test_loss, iter)
+    plot!(plt, loss, color = COLORS.loss, label = "training loss",
+          title = string("Loss Profiles| Loss:", @sprintf("%.4e", loss[end]), "|",
+                          "Test_Loss:", @sprintf("%.4e", test_loss[end])), xlabel = "iteration",
+          yaxis = :log)
+    # plot!(plt, test_loss, color = COLORS.test_loss, label = "test loss")
+end
 
-    x, y, xy, true_h, tspan_train, u0, spinning_rate = static_data.x, static_data.y, static_data.xy, static_data.true_h, static_data.tspan_train, static_data.u0, static_data.spinning_rate
+function plot_phase_plane(plt, model, UDE_sol, static_data)
+    plot!(plt, model.data[1,:], model.data[2,:], 
+          label = "True Solution", lw = 2, color = COLORS.true_solution,
+          xlabel = "x", ylabel = "y",
+          xlim = (0, 4.5), ylim = (0, 4.5),
+          title = "Phase Plane")
+    plot!(plt, UDE_sol[:,2], UDE_sol[:,3], 
+          label = "KAN Prediction", lw = 2, color = COLORS.prediction, linestyle = :dash)
+    scatter!(plt, [static_data.u0[1]], [static_data.u0[2]], color = COLORS.initial_condition, label = "Initial Condition")
+end
 
+function plot_time_series(plt, model, UDE_sol, static_data)
+    plot!(plt, model.times, model.data[1,:], label = "True x", color = COLORS.timeseries_x, lw = 2)
+    plot!(plt, model.times, model.data[2,:], label = "True y", color = COLORS.timeseries_y, lw = 2)
+    plot!(plt, UDE_sol[:,1], UDE_sol[:,2], label = "UDE Solution w/ KAN x", color = COLORS.timeseries_x, linestyle = :dash, lw = 2)
+    plot!(plt, UDE_sol[:,1], UDE_sol[:,3], label = "UDE Solution w/ KAN y", color = COLORS.timeseries_y, linestyle = :dash, lw = 2)
+    vspan!(plt, [static_data.tspan_train[1], static_data.tspan_train[2]], alpha = 0.2, color = COLORS.training_region, label = "Training Region",
+           xlabel = "Time", ylabel = "Population",
+           ylim = (0, 6),
+           title = "Time Series Comparison", legend = :topright)
+end
+
+function save_training_frame_2d(static_data::StaticData_2D, model::UDE, nn, pM, iter, loss, test_loss, training_dir; save = false)
+    @suppress begin
+        # KAN prediction
+        nn_h = [nn([i, j], pM, stM)[1][] for (i, j) in static_data.xy]
+
+        # Get predictions for trajectories
+        UDE_sol = UniversalDiffEq.forecast(model, static_data.u0, model.times)
+        # Create 2x2 grid plot
+        plt = plot(layout = (2, 2), size = (1600, 1200), titlefontsize = 12)
+
+        # Top row: Interaction function surfaces
+        plot!(plt[1], static_data.x, static_data.y, static_data.true_h, st = :surface, 
+              title = "True Interaction h(x,y) compared with KAN (Iteration $iter)",
+              xlabel = "x", ylabel = "y", zlabel = "h(x,y)",
+              alpha = 0.4, c = :blues,
+              camera = (iter * static_data.spinning_rate, 30))
+        
+        plot!(plt[1], static_data.x, static_data.y, nn_h, st = :surface, c = :blues,
+              alpha = 0.4, label = "KAN")
+    
+        # Top right: loss
+        plot_loss(plt[2], loss, test_loss, iter)
+
+        # Bottom left: Phase plane
+        plot_phase_plane(plt[3], model, UDE_sol, static_data)
+    
+        # Bottom right: Time series comparison
+        plot_time_series(plt[4], model, UDE_sol, static_data)
+
+        if save
+            # Save figure with iteration number
+            savefig(plt, joinpath(training_dir, "frame_$(lpad(iter, 5, '0')).png"))
+        end
+    
+        return plt
+    end
+end
+
+function save_training_frame_1d(static_data::StaticData_1D, model::UDE, nn, pM, iter, loss, test_loss, training_dir; save = false)
     # KAN prediction
-    nn_h = [nn([i,j], pM, stM)[1][] for (i,j) in  static_data.xy]
+    nn_h = [nn([i], pM, stM)[1][] for (i) in static_data.x]
 
     # Get predictions for trajectories
-    true_sol = model.data
-    UDE_sol = UniversalDiffEq.forecast(model,static_data.u0, model.times)
-    print(UDE_sol)
+    UDE_sol = UniversalDiffEq.forecast(model, static_data.u0, model.times)
     # Create 2x2 grid plot
-    plt = plot(layout=(2,2), size=(1600, 1200), titlefontsize=12)
+    plt = plot(layout = (2, 2), size = (1600, 1200), titlefontsize = 12)
     
+    x = static_data.x
     # Top row: Interaction function surfaces
-    plot!(plt[1], static_data.x, static_data.y, static_data.true_h, st=:surface, 
-          title="True Interaction h(x,y) compared with KAN (Iteration $iter)",
-          xlabel="x", ylabel="y", zlabel="h(x,y)",
-          alpha = 0.2, c=:blues,
-            camera=(iter*static_data.spinning_rate, 30))
-    
-    plot!(plt[1], static_data.x,  static_data.y, nn_h, st=:surface, c=:blues,
-            label="KAN")
- 
+    plot!(plt[1], x, x .* (1 .- x),
+          title = "True Interaction: h(x) = x(1-x)/10",
+          xlabel = "x", ylabel = "y",
+          label = "True h(x)",
+          color = :black,
+          xlim = (-1, static_data.sol_max_x + 1))
+    plot!(plt[1], x, nn_h, 
+          title = "KAN Learned Interaction (Iteration $iter)",
+          label = "KAN(x,θ)",
+          color = :red,
+          linestyle = :dash)
+    vspan!(plt[1], [0,static_data.sol_max_x], alpha = 0.2, color = :gray, label = "State Space")
+
     # Top right: loss
-    plot!(plt[2], loss, color = loss_color, label = "training loss",
-      title = string("Loss Profiles| Loss:", @sprintf("%.4e", loss[end]), "|",
-                      "Test_Loss:", @sprintf("%.4e", test_loss[end])), xlabel = "iteration",
-      yaxis=:log)
-    #plot!(plt[2], test_loss, color = test_loss_color , label = "test loss")
+    plot_loss(plt[2], loss, test_loss, iter)
 
     # Bottom left: Phase plane
-    plot!(plt[3], model.data[1,:], model.data[2,:], 
-          label="True Solution", lw=2, color=:black,
-          xlabel="x", ylabel="y",
-          xlim=(0,4.5),ylim=(0,4.5),
-           title="Phase Plane")
-    plot!(plt[3], UDE_sol[:,2], UDE_sol[:,3], 
-          label="KAN Prediction", lw=2, color=:royalblue1, linestyle=:dash)
-    scatter!(plt[3], [static_data.u0[1]], [static_data.u0[2]], color=:blue, label="Initial Condition")
+    plot_phase_plane(plt[3], model, UDE_sol, static_data)
     
     # Bottom right: Time series comparison
-    plot!(plt[4], model.times, model.data[1,:], label="True x",color=:lightskyblue2, lw=2)
-    plot!(plt[4], model.times, model.data[2,:], label="True y",color=:royalblue1, lw=2)
-    plot!(plt[4], UDE_sol[:,1], UDE_sol[:,2],   label="KAN x", color=:lightskyblue2, linestyle=:dash, lw=2)
-    plot!(plt[4], UDE_sol[:,1], UDE_sol[:,3],   label="KAN y", color=:royalblue1, linestyle=:dash, lw=2)
-    vspan!(plt[4], [static_data.tspan_train[1], static_data.tspan_train[2]], alpha=0.2, color=:gray, label="Training Region")
-    #=plot!(plt[4], 
-        xlabel="Time", ylabel="Population",
-        ylim=(0,6),
-        title="Time Series Comparison", legend=:topright) =#
-    if save
-        # Save figure with iteration number
-        savefig(plt, joinpath(training_dir, "frame_$(lpad(iter, 5, '0')).png"))
-    end
-    
-    return plt
-end
-#=
-function save_training_frame_1d(p, iter, loss, test_loss,training_dir; save=false)
-    # Get predictions for trajectories
+    plot_time_series(plt[4], model, UDE_sol, static_data)
 
-    kan_sol = predict_test(p)
-    #kan_train_sol = predict(p)
-    kan_h = [kan1([i],ComponentArray(p,pM_axis),stM)[1][1] for i in x]
-    # Create 2x2 grid plot
-    plt = plot(layout=(2,2), size=(1600, 1200), titlefontsize=12)
-    
-    # Top row: Interaction function surfaces
-    plot!(plt[1], x, x.*(1 .- x),
-            title="True Interaction: h(x)=x(1-x)/10",
-            xlabel="x", ylabel="y",
-            label="True h(x)",
-            color=:black,
-            xlim=(-1,sol_max_x+1))
-    plot!(plt[1], x, kan_h, 
-            title="KAN Learned Interaction (Iteration $iter)",
-            label = "KAN(x,θ)",
-            color=:red,
-            linestyle=:dash)
-    vspan!(plt[1],[0,sol_max_x],alpha=0.2, color=:gray, label="State Space")
-  # Top right: loss
-    plot!(plt[2], loss, color = loss_color, label = "training loss",
-            title = string("Loss Profiles| Loss:", @sprintf("%.4e", loss[end]), "|",
-                            "Test_Loss:", @sprintf("%.4e", test_loss[end])), xlabel = "iteration",
-            yaxis=:log)
-    plot!(plt[2], test_loss, color = test_loss_color , label = "test loss")
-    # Bottom left: Phase plane
-    plot!(plt[3], true_sol[1,:], true_sol[2,:], 
-          label="True Solution", lw=2, color=:black,
-          xlabel="x", ylabel="y",
-          #xlim=(0,4.5),ylim=(0,4.5),
-           title="Phase Plane")
-    plot!(plt[3], kan_sol[1,:], kan_sol[2,:], 
-          label="KAN Prediction", lw=2, color=:red, linestyle=:dash)
-    scatter!(plt[3], [u0[1]], [u0[2]], color=:blue, label="Initial Condition")
-    
-    # Bottom right: Time series comparison
-    plot!(plt[4], t_test, true_sol[1,:], label="True x", color=timeseries_x_color, lw=2)
-    plot!(plt[4], t_test, true_sol[2,:], label="True y", color=timeseries_y_color, lw=2)
-    plot!(plt[4], kan_sol.t, kan_sol[1,:], label="KAN x", color=timeseries_x_color, linestyle=:dash, lw=2)
-    plot!(plt[4], kan_sol.t, kan_sol[2,:], label="KAN y", color=timeseries_y_color, linestyle=:dash, lw=2)
-    vspan!(plt[4], [tspan_train[1], tspan_train[2]], alpha=0.2, color=:gray, label="Training Region")
-    plot!(plt[4], 
-        xlabel="Time", ylabel="Population",
-        title="Time Series Comparison", legend=:topright)
     if save
         # Save figure with iteration number
         savefig(plt, joinpath(training_dir, "frame_$(lpad(iter, 5, '0')).png"))
     end
     return plt
 end
-=#
--- KANODE/UniversalDiffEq/uniDiff_KAN_1d.jl
+++ KANODE/UniversalDiffEq/uniDiff_KAN_1d.jl
@@ -6,17 +6,18 @@ using OrdinaryDiffEq
 using UniversalDiffEq, DataFrames
 using ProgressBars, Printf, Plots
 using Optimization, Optimisers
+
 include("../Lotka-Volterra/src/KolmogorovArnold.jl")
 using .KolmogorovArnold
 include("plotting_functions.jl")
 
 
-function h(x,y)
-    0.5*x*y
+function h(x,K)
+    x*(1-x/K)
 end
 function lotka!(du,u,p,t) 
-    du[1] = p.r*u[1] - h(u[1],u[2])
-    du[2] = h(u[1],u[2]) - p.m*u[2]
+    du[1] = p.r*h(u[1],p.K) - 0.5*u[1]*u[2]
+    du[2] =  0.5*u[1]*u[2] - p.m*u[2]
 end
 println("Generating data")
 #data generation parameters
@@ -24,7 +25,7 @@ dt=0.1
 tspan = (0.0, 14)
 tspan_train=(0.0, 14)
 u0 = [1.0f0, 1.0f0]
-p_= (r= 1.0f0, m=1.0f0)
+p_= (r= 1.0f0, m=1.0f0, K=10.0f0)
 prob = ODEProblem(lotka!, u0, tspan, p_)
 solution = solve(prob, Tsit5(), abstol = 1e-12, reltol = 1e-12, saveat = dt, verbose = false )
 
@@ -39,12 +40,12 @@ data = DataFrame(time=times, prey=u1, predator=u2)
 println("Setting up KAN")
 ##Model definition
 basis_func = KolmogorovArnold.rbf      # rbf, rswaf
-normalizer = softsign # sigmoid(_fast), tanh(_fast), softsign
+normalizer = KolmogorovArnold.softsign # sigmoid(_fast), tanh(_fast), softsign
 num_layers=2 #defined just to save into .mat for plotting
 layer_width=7
 grid_size=5
 kan1 =Lux.Chain(
-    KDense( 2, layer_width, grid_size; use_base_act = true, basis_func, normalizer),
+    KDense( 1, layer_width, grid_size; use_base_act = true, basis_func, normalizer),
     KDense(layer_width,  1, grid_size; use_base_act = true, basis_func, normalizer),
 )
 # initialize the neural network states and parameters
@@ -54,23 +55,28 @@ Random.seed!(rng, 3)
 pM, stM = Lux.setup(rng,kan1)
 # Define the UDE
 function lotka_voltera!(du,u,p,t)
-    C, _ = kan1(u,pM,stM)
-    du[1] = p.r*u[1] - C[1]
-    du[2] = C[1] - p.m*u[2]
+    C, _ = kan1([u[1]],pM,stM)
+    du[1] = 1.0*C[1] -  0.5*u[1]*u[2]
+    du[2] =  0.5*u[1]*u[2] - 1.0*u[2]
 end
-
-initial_parameters = (NN=pM, r=2.0, m=0.5)
-model = CustomDerivatives(data, lotka_voltera!, initial_parameters, obs_weight = 0, reg_weight = 0)
+#=
+function lotka_voltera!(du,u,p,t)
+    C, _ = kan1([u[1]],pM,stM)
+    du[1] = p.r*C[1] -  0.5*u[1]*u[2]
+    du[2] =  0.5*u[1]*u[2] - p.m*u[2]
+end
+=#
+initial_parameters = (NN=pM)#, r=2.0, m=0.5, K=10.0)
+model = CustomDerivatives(data, lotka_voltera!, initial_parameters)
 ##Plotting
 #Bounds on the interaction plot
 x = range(0, 3, length=20)
-y = range(0, 3, length=20)
-xy = [(i,j) for i in x, j in y]
 # True interaction function   
-true_h = [h(i,j) for (i,j) in xy]
+true_h = [h(i,p_.K) for i in x]
 #How fast the interaction plot spis
-spinning_rate=0.5
-static_data = StaticData(x,y,xy,true_h,tspan_train, u0, spinning_rate)
+spinning_rate=0.2
+sol_max_x = maximum(data.prey)
+static_data = StaticData_1D(x,true_h,tspan_train, u0, spinning_rate, sol_max_x)
 
 
 ##Training
@@ -90,21 +96,13 @@ end
 print("Starting Training")
 @time for i in iterator
 
-    #Update model
-    # set optimization problem 
-    target  = (x,p) -> model.loss_function(x)
-    adtype  = Optimization.AutoZygote()
-    optf    = Optimization.OptimizationFunction(target, adtype)
-    optprob = Optimization.OptimizationProblem(optf, model.parameters)
-
-    # run optimizer for 10 steps
-    print("Time to train $iters_per_loop steps")
-    @time sol = Optimization.solve(optprob, 
-                                Optimisers.Adam(0.05), 
-                                maxiters = iters_per_loop+1)
-    
-    # assign parameters to model 
-    model.parameters = sol.u
+    UniversalDiffEq.train!(model, 
+                            loss_function = "multiple shooting",
+                            verbose = false,
+                            regularization_weight = 0,
+                            optimizer = "ADAM",
+                            optim_options = (maxiter = 2, step_size = 1e-2)
+                            )
 
 
     loss_curr=deepcopy(model.loss_function(model.parameters))
@@ -114,15 +112,10 @@ print("Starting Training")
     append!(l_test, [loss_curr_test])
 
     # CALLBACK
-
-    #loss_curr_test=deepcopy(loss_test(p))plt = plot(layout=(2,2), size=(1600, 1200), titlefontsize=12)
-    print("Time to print loss")
-    @time set_description(iterator, string("Iter:", i, "| Loss:", @sprintf("%.2f", loss_curr), "|"))
-    println("Time to render graphics")
-    @time display(save_training_frame_2d(static_data, model, kan1, pM, i*iters_per_loop,l,l_test, training_dir; save=SAVE_ON))
-    #display(UniversalDiffEq.plot_state_estimates(model))
-    #display(UniversalDiffEq.plot_predictions(model))
-    # SAVE
-    #callback(i)
-    println("")
+    #print("Time to print loss")
+    set_description(iterator, string("Iter:", i, "| Loss:", @sprintf("%.2f", loss_curr), "|"))
+    print(pM)
+    #println("Time to render graphics")
+    #UniversalDiffEq.plot_state_estimates(model)
+    @time display(save_training_frame_1d(static_data, model, kan1, pM, i*iters_per_loop,l,l_test, training_dir; save=SAVE_ON))
 end
