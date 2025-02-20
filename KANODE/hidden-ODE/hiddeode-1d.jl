c34cedfe6674289f64c2099374681f0d2fbbed7c
macquarrielucas <macquarrielucas@gmail.com>
2025-02-20 14:20:57 -0400

    multishooting method is implemented but is sosososo slow.

--- KANODE/hidden-ODE/hiddeode-1d.jl
+++ KANODE/hidden-ODE/hiddeode-1d.jl
@@ -1,6 +1,8 @@
 # PACKAGES AND INCLUSIONS
 using ChainRulesCore
 using ComponentArrays
+using ComponentArrays: getaxes
+using ComponentArrays: getdata
 using ConcreteStructs
 using DiffEqFlux
 using Flux
@@ -19,12 +21,14 @@ using ProgressBars
 using Random
 using WeightInitializers
 using Zygote
+using Lux
 pythonplot()
 
-include("plotting_functions.jl")
+include("../helpers/plotting_functions.jl")
+include("loss_functions.jl")
 # DIRECTORY
 dir         = @__DIR__
-dir         = dir*"/"
+dir         = "$dir/"
 cd(dir)
 add_path    = "test/"
 #=
@@ -40,8 +44,13 @@ using .KolmogorovArnold
 rng = Random.default_rng()
 Random.seed!(rng, 3)
 
+function h(x)
+    #The hidden function that we are trying to learn
+    x*(1-x)
+end
+
 function lotka!(du,u,p,t) 
-    du[1] = u[1]*(1-u[1]) - 0.5*u[1]*u[2]
+    du[1] = h(u[1])- 0.5*u[1]*u[2]
     du[2] = 0.5*u[1]*u[2] - 0.03*u[2]
 end
 
@@ -55,27 +64,20 @@ prob = ODEProblem(lotka!, u0,tspan_test,p_)
 
 #generate training data, split into train/test
 solution = solve(prob, Tsit5(), abstol = 1e-12, reltol = 1e-12, saveat = dt, verbose = false)
-end_index=Int64(floor(length(solution.t)*tspan_train[2]/tspan_test[2])) + 1
+# Calculate the index to split the solution into training and test sets based on the training time span
+end_index = Int64(floor(length(solution.t) * tspan_train[2] / tspan_test[2])) + 1
 t_test = solution.t #full dataset
 t_train=t_test[1:end_index] #training cut
 X = Array(solution)
 Xn = deepcopy(X) 
 plot(solution)
-
-
-#   Next we'll define the KAN.
-
+# Define KAN
 basis_func = rbf      # rbf, rswaf
 normalizer = softsign # sigmoid(_fast), tanh(_fast), softsign
-
-# Define KAN-ODEs
-###layer_width and grid_size can be modified here to replicate the testing in section A2 of the manuscript
-
-
 layer_width=5
 grid_size=10
 #This KAN looks like
-# phi1(x) + phi2(x) + phi3(x) + ... + phi10(x) 
+# psi(phi1(x) + phi2(x) + phi3(x) + ... + phi10(x))
 kan1 = Lux.Chain(
     KDense( 1, layer_width, grid_size; use_base_act = true, basis_func, normalizer),
     KDense(layer_width,  1, grid_size; use_base_act = true, basis_func, normalizer),
@@ -85,10 +87,6 @@ pM_data     = getdata(ComponentArray(pM))
 pM_axis     = getaxes(ComponentArray(pM))
 p = ComponentArray(pM_data, pM_axis) 
 #p = (deepcopy(pM_data))./1e5 ;
-
-
-#   and construct the KAN-ODE. We'll also define the prediction function which
-#   will simulate the KAN-ODEs
 
 # CONSTRUCT KAN-ODES
 function kanode!(du, u, p, t)
@@ -108,10 +106,45 @@ function predict_test(p)
     sol = solve(prob, Tsit5(), verbose = false);
 end
 
-# LOSS FUNCTION
+function multiple_shooting_loss(p, pred_length::Int, times::Vector{<:AbstractFloat})::Real
 
+    #Solve the ODE on the subinterval \tau_j to \tau_{j+1}
+    function predict_mini(u,tsteps,parameters)
+        tspan =  (tsteps[1],tsteps[end])  # Tsit5()#ForwardDiffSensitivity()
+        prob=ODEProblem(kanode!, u, tspan, parameters, saveat=tsteps)
+        sol = OrdinaryDiffEq.solve(prob, abstol=1e-6, reltol=1e-6)
+        X = Array(sol)
+        return X
+    end 
 
-function reg_loss(p, act_reg=1.0, entropy_reg=1.0)
+    #Calculate the subinternals then the loss
+    loss = 0 
+    inds = 1:pred_length
+    tspan = 1:pred_length
+    for tau in 1:pred_length:(size(times)[1])
+        #If the interval is in the bounds of times
+        if ( size(times)[1]-tau) >= pred_length
+            inds = tau:(tau+pred_length)
+            tspan = times[inds]
+        #If the interval would be greater than the size of times
+        else
+            inds = tau:size(times)[1]
+            tspan = times[inds]
+        end 
+        #The tau'th point of the data matrix
+        u0 = Xn[:,tau]
+        #The data matrix for the interval
+        u1 = Xn[:,inds]
+        u1hat = predict_mini(u0,tspan,p)
+        for i in 1:length(inds) 
+            loss += sum((u1[:,i].-u1hat[:,i]).^2)/length(Xn)
+        end 
+    end
+    return loss
+end
+
+# LOSS FUNCTIONS
+function reg_loss(p, act_reg=1.0, entropy_reg=1.0)::Real
     l1_temp=(abs.(p))
     activation_loss=sum(l1_temp)
     #This entropy was not mentioned in the paper i believe,
@@ -122,10 +155,15 @@ function reg_loss(p, act_reg=1.0, entropy_reg=1.0)
     return total_reg_loss
 end
 
+
+function single_shooting_loss(p)::Real
+    mean(abs2, Xn[:, 1:end_index].- predict(p)) 
+end
 #overall loss
 sparse_on = 0
-function loss_train(p)
-    loss_temp=mean(abs2, Xn[:, 1:end_index].- predict(p))
+function loss_train(p)::Real
+    #loss_temp=single_shooting_loss(p)
+    loss_temp=multiple_shooting_loss(p, 5, t_train)
     if sparse_on==1
         loss_temp+=reg_loss(p, 5e-4, 0) #if we have sparsity enabled, add the reg loss
     end
@@ -136,52 +174,43 @@ function loss_train(p)
     mean(abs2, Xn[:, 1:end_index] .- Array(predict(p)))
 end
 =#
-function loss_test(p)
+function loss_test(p)::Real
     mean(abs2, Xn .- Array(predict_test(p)))
 end
 
-# Create grid for interaction function visualization
 
-sol_max = maximum(vcat(solution.u...)) #Max of x for plotting
+##Plotting
 sol_max_x =maximum([x[1] for x in solution.u]) 
-x = collect(range(-1, sol_max_x+1, length=40))
-true_sol = solution  # Higher resolution for smooth plot
-#y = range(0, 3, length=20)
-#xy = [(i,j) for i in x, j in y]
+#Bounds on the interaction plot
+x = range(-1, sol_max_x+1, length=40)
+# True interaction function   
+true_h = [h(i) for i in x]
 
-# True interaction function
-#true_h = [0.5*i*j for (i,j) in xy]
+observation_data = [solution.t [u[1] for u in solution.u] [u[2] for u in solution.u]]
+static_data = StaticData_1D(observation_data, 
+                            x,
+                            true_h,
+                            tspan_train, 
+                            u0, 
+                            sol_max_x)
 
-# KAN prediction
-#kan_h = [kan1([i,j], ComponentArray(p,pM_axis), stM)[1][] for (i,j) in xy]
 
 
-
-SAVE_ON = true
-training_dir = nothing
+SAVE_ON = false
 if SAVE_ON
-    # Create directories if needed
-    folder_count = 0
-    training_dir = joinpath(dir, add_path, "training_frames_$folder_count")
-    is_empty = isempty(readdir(training_dir))
-    #If its already a directory and its not empty, try the next folder
-    while isdir(training_dir) && !is_empty 
-        global folder_count += 1 
-        global training_dir = joinpath(dir, add_path, "training_frames_$folder_count")
-    end
-    if !is_empty
-        print("Making directory ", string(training_dir))
-        mkdir(training_dir)
-    end
+    training_dir = find_frame_directory()
+else
+    training_dir=""
 end
 #opt = Flux.Momentum(1e-3, 0.9)
 opt = Flux.Adam(1e-4)
 N_iter = 10000
 iterator = ProgressBar(1:N_iter)
-l = []
-l_test=[]
-p_list = []
 
+#Stuff to track loss and test loss
+l = Real[]
+l_test=Real[]
+p_list = []
 @time for i in iterator
     # GRADIENT COMPUTATION
     grad = Zygote.gradient(loss_train, p)[1]
@@ -193,16 +222,24 @@ p_list = []
     # CALLBACK
     loss_curr=deepcopy(loss_train(p))
     loss_curr_test=deepcopy(loss_test(p))
-    set_description(iterator, string("Iter:" ,i, "| Loss:", @sprintf("%.2f", loss_curr), "|",
-                            "Test_Loss:", @sprintf("%.2f", loss_curr_test), "|"))
+    set_description(iterator, string(
+        "Iter:", i, 
+        "| Loss:", @sprintf("%.2f", loss_curr), 
+        "| Test_Loss:", @sprintf("%.2f", loss_curr_test), 
+        "|"
+    ))
 
-    append!(l, [loss_curr])
-    append!(l_test, [loss_curr_test])
+    append!(l, loss_curr)
+    append!(l_test, loss_curr_test)
     #append!(p_list, [deepcopy(p)])
 
+
     if i%10 == 0 || i==1
+        #Turn the data into an nx3 matrix for the plotting function
+        UDE_forecast = predict_test(p)
+        UDE_sol=[UDE_forecast.t [u[1] for u in UDE_forecast.u] [u[2] for u in UDE_forecast.u]]
         print(training_dir)
-        @time display(save_training_frame_1d(p,i, l,l_test,training_dir; save=SAVE_ON))
+        @time display(save_training_frame(static_data, UDE_sol, kan1,p,i, l,l_test,training_dir; save=SAVE_ON))
     end
     # SAVE
     #callback(i)
