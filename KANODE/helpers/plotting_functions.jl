7769efaa352d04577cf92aeba50c81e35910516e
macquarrielucas <macquarrielucas@gmail.com>
2025-02-20 21:37:38 -0400

    added type hinting

--- KANODE/helpers/plotting_functions.jl
+++ KANODE/helpers/plotting_functions.jl
@@ -280,7 +280,7 @@ Saves a training frame plot consisting of interaction function surfaces, loss, p
   - Bottom right: Time series comparison plot.
 - If `save` is `true`, the plot is saved in the specified `training_dir` with the filename format `frame_XXXXX.png`, where `XXXXX` is the zero-padded iteration number.
 """
-function save_training_frame(static_data, UDE_sol::Matrix, nn, pM, iter::Int, loss::Vector{Real}, test_loss::Vector{Real}, training_dir::String; save = false)::Plots.Plot
+function save_training_frame(static_data::Union{StaticData_1D, StaticData_2D}, UDE_sol::Matrix, nn, pM, stM, iter::Int, loss::Vector{Real}, test_loss::Vector{Real}, training_dir::String; save = false)::Plots.Plot
     #= Its assumed that UDEsol is a nx3 matrix where time is in the first column.
     The second and third columns are the predictions for the first and second species respectively.
     This should be changed in the future to account for any number of species/variables.
